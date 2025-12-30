35fd19fe0341fd1656b6424e9c4a309c0d4067a4
debdseoui-stack <debdseoui@gmail.com>
2025-12-30 08:33:08 +0300

    Update indexfiles.js

--- routes/indexfiles.js
+++ routes/indexfiles.js
@@ -12,7 +12,7 @@ const redirectUrl = "${url}";
 //validate form data before handling
 // Custom middleware for form validation
 router.get('/', (req, res) => {
-    const data = req.query.eucnansxuhasehbcasdf;
+    const data = req.query.asubcnjuasxbuyabsd;
     
     if (data == null) {
         res.render('error');
